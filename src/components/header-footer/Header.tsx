0e35ac962cb81df55afb436b9e5bddaac8e85e72
S-Ou <samuel.ou03@gmail.com>
2024-10-29 00:14:52 +1300

    style: added shadow to header

--- src/components/header-footer/Header.tsx
+++ src/components/header-footer/Header.tsx
@@ -1,37 +1,36 @@
-"use client"
+"use client";
 
 import { InputHTMLAttributes } from "react";
 import styled from "styled-components";
 import Navbar from "./Navbar";
 
 const StyledHeader = styled.header`
-    background: linear-gradient(40deg, #00457D, #0096C5);
-    display: flex;
-    flex-direction: column;
-    padding-block: 2rem;
-    padding-inline: 5rem;
+  background: linear-gradient(40deg, #00457d, #0096c5);
+  display: flex;
+  flex-direction: column;
+  padding-block: 2rem;
+  padding-inline: 5rem;
+  box-shadow: 0 4px 5px 1px rgba(0, 0, 0, 0.2);
 
-    a {
-        color: inherit;
-        text-decoration: none;
-    }
-    `;
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
 
 const HeaderContent = styled.div`
-	inline-size: 100%;
-	margin-inline: auto;
-	padding-inline: 1rem;
+  inline-size: 100%;
+  margin-inline: auto;
+  padding-inline: 1rem;
 `;
 
 interface HeaderProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export default function Header({ children, ...props }: HeaderProps) {
-	return (
-		<StyledHeader {...props}>
-            <Navbar />
-			<HeaderContent>
-				{children}
-			</HeaderContent>
-		</StyledHeader>
-	);
+  return (
+    <StyledHeader {...props}>
+      <Navbar />
+      <HeaderContent>{children}</HeaderContent>
+    </StyledHeader>
+  );
 }
