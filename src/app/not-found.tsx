eb3b0ebe3464272989f1efe866dbaf41f16f33e8
S-Ou <samuel.ou03@gmail.com>
2024-10-29 03:21:13 +1300

    feat: 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import styled from "styled-components";
+
+const NotFound = styled.div`
+  margin-block: 5rem;
+  padding-inline: 10rem;
+  text-align: center;
+
+  h1 {
+    background: linear-gradient(40deg, #00457d, #0096c5);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 10rem;
+    font-stretch: expanded;
+    font-weight: 900;
+  }
+
+  h2 {
+    font-size: 3rem;
+  }
+`;
+
+export default function Custom404() {
+  return (
+    <NotFound>
+      <h1>404</h1>
+      <h2>Page Not Found</h2>
+    </NotFound>
+  );
+}
--- src/components/header-footer/Footer.tsx
+++ src/components/header-footer/Footer.tsx
@@ -4,12 +4,15 @@ import styled from "styled-components";
 
 const StyledFooter = styled.footer`
   background: linear-gradient(40deg, #00457d, #0096c5);
+  bottom: 0;
   color: white;
   display: flex;
   flex-direction: column;
   gap: 2rem;
   padding-block: 2rem;
   padding-inline: 10rem;
+  position: absolute;
+  width: 100%;
 `;
 
 export default function Footer() {
