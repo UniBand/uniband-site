411aa50a29d51190a0f217d1229002b57ed5821c
S-Ou <samuel.ou03@gmail.com>
2024-10-30 15:16:20 +1300

    feat: youtube link

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -8,10 +8,43 @@ const VideoContainer = styled.div`
   padding-inline: var(--body-margins);
 `;
 
+const ButtonWrap = styled.div`
+  display: flex;
+  gap: 2rem;
+  justify-content: center;
+  margin-bottom: 2rem;
+`;
+
+const YouTubeButton = styled.button`
+  background: var(--background);
+  border-radius: 0.5rem;
+  border: var(--dark-blue) 2px solid;
+  color: var(--dark-blue);
+  cursor: pointer;
+  font-size: 1.25rem;
+  font-stretch: expanded;
+  padding-block: 0.5rem;
+  padding-inline: 1rem;
+  transition: background 400ms ease, color 400ms ease;
+  width: fit-content;
+
+  &:hover {
+    background: var(--dark-blue);
+    color: var(--background);
+  }
+`;
+
 export default function Videos() {
   return (
     <>
       <SpacedTitle>Videos</SpacedTitle>
+      <ButtonWrap>
+        <YouTubeButton>
+          <a href="https://www.youtube.com/channel/UCy_Eez7ZamDM31jOTRvqrWw">
+            Find us on YouTube
+          </a>
+        </YouTubeButton>
+      </ButtonWrap>
       <VideoContainer>
         <VideosComponent />
       </VideoContainer>
