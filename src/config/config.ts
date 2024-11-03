9f1a88d6add889f67d1e1b0275c261b53012f499
S-Ou <samuel.ou03@gmail.com>
2024-11-04 04:06:22 +1300

    refactor: updated text slightly

--- src/config/config.ts
+++ src/config/config.ts
@@ -29,7 +29,7 @@ export const UniBandConfig = {
     infoHeader:
       "**UniBand** is the University of Auckland’s leading Concert Band",
     infoContent: [
-      "UniBand provides an opportunity for wind, brass, and percussion musicians from all across the university to come together and make music.",
+      "UniBand provides an opportunity for wind, brass, and percussion musicians studying all degrees in all universities to make music together.",
       "We rehearse weekly on Thursday evenings and perform in concerts and competitions throughout the year.",
     ],
     infoImage: {
@@ -40,10 +40,10 @@ export const UniBandConfig = {
   about: {
     aboutText: [
       "**UniBand** is a student-founded and student-run concert band in Auckland. Founded in 2013, UniBand has been providing university students an opportunity to play challenging, yet fulfilling music with peers.",
-      "Unlike most other university music groups which strongly prioritise students studying Music, UniBand is an all-comers band; open to musicians studying any degree, with many members studying degrees such as Engineering and Law.",
+      "Unlike most other university music groups which strongly prioritise students studying music, UniBand is an all-comers band; open to musicians studying any degree, with many members studying degrees such as engineering, law, and others.",
       "UniBand performs at a variety of events throughout the year, including self-hosted concerts, university events, and the annual NZCBA Concert Band Festival.",
       "The heart of UniBand is the tight social community at its core. With a welcoming social environment and multiple casual events throughout the year, members form lasting friendships and connections.",
-      "Rehearsals are held weekly on Thursdays evenings from 7-9pm on campus during the University of Auckland semester.",
+      "Rehearsals are held weekly on Thursdays evenings from 7-9pm on UoA campus during the University of Auckland semester.",
     ],
     aboutImage: {
       path: "about/blurb-img.jpg",
