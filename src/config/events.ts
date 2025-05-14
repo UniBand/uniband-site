ec733d9ecf4d0989701722265b6281a087b4fd32
Anthony Wang <anthonydrwnz@gmail.com>
2025-05-14 22:33:26 +1200

    src/config/events.ts

--- src/config/events.ts
+++ src/config/events.ts
@@ -10,6 +10,15 @@ import {
 
 export const UniBandEvents: Event[] = [
     // Self-hosted performances
+
+    {
+      title: "Fire and Fable",
+      startDate: "2025-06-1",
+      description:
+        "The 2025 Semester 1 concert, performed by UniBand and featuing Uniband's Jazz Band. Directed by Jono Palmer with guest conductors Sue Lynn Leong, Athena Shiu, and Christopher Todd",
+      imagePath: "events/Fire and Fable.png",
+      location: "Dorothy Winstone Centre ",
+    },
     {
       title: "Glorious Heights",
       startDate: "2024-10-12",
